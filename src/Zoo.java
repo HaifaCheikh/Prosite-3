ace7947c9966ce592f76aefde590c2de72a2761a
Haifa Cheikh <Haifa.Cheikh@esprit.tn>
2024-10-01 15:36:25 +0200

    prosite 3

--- src/Zoo.java
+++ src/Zoo.java
@@ -2,20 +2,19 @@ public class Zoo {
     private Animal[] animals;
     private String name;
     private String city;
-    private final int nbrCages = 25; // Nombre constant de cages
-    private int animalCount; // Compteur d'animaux
+    private final int nbrCages = 25;
+    private int animalCount;
 
     public Zoo(String name, String city) {
         this.name = name;
         this.city = city;
-        this.animals = new Animal[nbrCages]; // Le tableau d'animaux
-        this.animalCount = 0; // Initialisation du compteur d'animaux
-    }
+        this.animals = new Animal[nbrCages];
+        this.animalCount = 0;}
 
     public boolean addAnimal(Animal animal) {
         if (!isZooFull() && !animalExists(animal)) {
-            animals[animalCount] = animal; // Ajout de l'animal
-            animalCount++; // Incrémentation du compteur
+            animals[animalCount] = animal;
+            animalCount++;
             return true;
         }
         System.out.println("Pas assez de cages ou animal déjà présent.");
@@ -25,23 +24,23 @@ public class Zoo {
     public boolean removeAnimal(Animal animal) {
         for (int i = 0; i < animalCount; i++) {
             if (animals[i].equals(animal)) {
-                animals[i] = animals[animalCount - 1]; // Remplace l'animal supprimé par le dernier
-                animals[animalCount - 1] = null; // Supprime la référence
-                animalCount--; // Décrémente le compteur
+                animals[i] = animals[animalCount - 1];
+                animals[animalCount - 1] = null;
+                animalCount--;
                 return true;
             }
         }
-        return false; // Animal non trouvé
+        return false;
     }
 
     public boolean isZooFull() {
-        return animalCount >= nbrCages; // Vérifie si le zoo est plein
+        return animalCount >= nbrCages;
     }
 
     public boolean animalExists(Animal animal) {
         for (int i = 0; i < animalCount; i++) {
             if (animals[i].equals(animal)) {
-                return true; // L'animal existe déjà dans le zoo
+                return true;
             }
         }
         return false;
@@ -57,14 +56,14 @@ public class Zoo {
     public int searchAnimal(Animal animal) {
         for (int i = 0; i < animalCount; i++) {
             if (animals[i].equals(animal)) {
-                return i; // Retourne l'indice de l'animal trouvé
+                return i;
             }
         }
         return -1; // Animal non trouvé
     }
 
     public static Zoo comparerZoo(Zoo z1, Zoo z2) {
-        return z1.animalCount >= z2.animalCount ? z1 : z2; // Retourne le zoo avec le plus d'animaux
+        return z1.animalCount >= z2.animalCount ? z1 : z2; 
     }
 
     @Override
